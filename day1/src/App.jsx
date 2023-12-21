cd1a7bd5f500d62f741cea0cc59aded68aaa07ca
Kachavi <vishaldineshbhaikacha@gmail.com>
2023-12-21 14:27:32 +0530

    day 2

---
+++ day1/src/App.jsx
@@ -0,0 +1,139 @@
+import { useState } from "react"
+import { useEffect } from "react"
+
+function App() {
+
+  const [data, setData] = useState();
+  const [catagories, setCatagories] = useState("");
+  const [filterData, setFilterData] = useState([]);
+  const [searchFilterData, setSearchFilterData] = useState("");
+
+  useEffect(() => {
+    datafetch();
+  }, [])
+
+  async function datafetch() {
+    const data = await fetch('https://advent.sveltesociety.dev/data/2023/day-one.json')
+    const res = await data.json();
+    setData([...res]);
+    setFilterData([...res]);
+  }
+
+  useEffect(() => {
+    searchName();
+  }, [searchFilterData])
+
+  useEffect(() => {
+    searchCatagories();
+  }, [catagories])
+
+
+  function searchCatagories() {
+    if (catagories === "Naughty") {
+      const naughty = data && data.filter(element => element.tally < 0);
+      setFilterData(naughty);
+    } else if (catagories === "Nice") {
+      const nice = data && data.filter(element => element.tally > 0)
+      setFilterData(nice);
+    } else {
+      setFilterData(data);
+    }
+  }
+
+  function searchName() {
+    const searchnamedata = data && data.filter(
+      element => element.name.toLocaleLowerCase()
+        .includes(searchFilterData.toLocaleLowerCase())
+    );
+
+    setFilterData(searchnamedata);
+  }
+
+
+  return (
+    <>
+      <div className="container">
+        <div>
+          <form>
+            <label htmlFor="search">
+              Search Name:
+              <input type="text" name="search" id="search"
+                onChange={e => {
+                  setSearchFilterData(e.target.value);
+                  searchName()
+                }}
+              />
+            </label>
+
+            <label htmlFor="catagories">
+              Search Catagories:
+              <select name="catagories" id="catagories"
+                onChange={e => {
+                  setCatagories(e.target.value);
+                }}
+              >
+                <option value=""></option>
+                <option value="Nice">Nice</option>
+                <option value="Naughty">Naughty</option>
+              </select>
+            </label>
+          </form>
+        </div>
+        <table>
+          <tr>
+            <th>Name's</th>
+            <th>Tally</th>
+            <th>Naughty or Nice</th>
+          </tr>
+          <List data={filterData} />
+        </table>
+      </div>
+    </>
+  )
+}
+
+export default App
+
+function List(params) {
+  return (
+    <>
+      {
+        params.data && params.data.map((element, index) => (
+          <>
+            <tr key={index}>
+              <td>{element.name}</td>
+              <td>{element.tally}</td>
+
+              {element.tally != null
+                ? <td>{element.tally < 0 ? <Naughty /> : <Nice />}</td>
+                : "-"
+              }
+            </tr>
+          </>
+        ))
+      }
+    </>
+  )
+}
+
+function Naughty() {
+
+  return (
+    <div>
+      <button className="naughty">
+        Naughty
+      </button>
+    </div>
+  )
+}
+
+function Nice() {
+
+  return (
+    <div>
+      <button className="nice">
+        Nice
+      </button>
+    </div>
+  )
+}
---
+++ day2/src/App.jsx
@@ -0,0 +1,57 @@
+import { useState, useRef } from "react";
+import cookie from "./assets/cookie.png";
+
+function App() {
+  const parent = useRef(null);
+  const [count, setCount] = useState(1);
+
+  function randomCookie() {
+    setCount(count + 1);
+    const randomLeft = Math.random() * window.innerWidth;
+    const randomTop = Math.random() * window.innerHeight;
+
+    const fragment = document.createDocumentFragment();
+
+    fragment
+      .appendChild(document.createElement("img"))
+      .setAttribute("src", cookie);
+
+    parent.current.appendChild(fragment);
+
+    const img = document.querySelectorAll("img");
+    img[img.length - 1].style.left = randomLeft + "px";
+    img[img.length - 1].style.top = randomTop + "px";
+  }
+
+  function removeCookie() {
+    if (count != 0) {
+      setCount(count - 1);
+    } else {
+      setCount(0);
+    }
+
+    const img = document.querySelectorAll("img");
+    if (img.length != 0) {
+      img[img.length - 1].remove();
+    }
+  }
+
+  return (
+    <>
+      <div className="counter">
+        <h1>Cookie Count: {count}</h1>
+        <div>
+          <button onClick={randomCookie}>+</button>
+          <button onClick={removeCookie}>-</button>
+        </div>
+      </div>
+      <div className="container-full" ref={parent}>
+        <div className="cookie-div">
+          <img src={cookie} alt="" width="200px" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
